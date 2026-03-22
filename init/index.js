500d1347f34767a8ee2d524c46455576a4791a7d
Himanshu <himanshuhima09@gmail.com>
2026-03-22 15:12:25 +0530

    Fixed MongoDB Atlas connection and added sample data

--- init/index.js
+++ init/index.js
@@ -1,28 +1,31 @@
+require("dotenv").config({ path: "../.env" });
+
 const mongoose = require("mongoose");
-const initData = require("./data.js");
 const Listing  = require("../models/listing.js");
 const smallInit = require("./small.js");
 
-const MONGO_URL  = "mongodb://127.0.0.1:27017/wanderlust";
-main()
-.then((res) => {
-    console.log("Connected to DB");
-}).catch((err) => {
-    console.log(err);
-});
+const dbUrl = process.env.ATLASDB_URL;
+
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    try {
+        await mongoose.connect(dbUrl);
+        console.log("Connected to DB");
+
+        await Listing.deleteMany({});
+        console.log("🗑 Old data deleted");
+
+        smallInit.data = smallInit.data.map((obj) => ({
+            ...obj,
+            owner: '6831fe2eef63a3852123d1a9'
+        }));
+
+        await Listing.insertMany(smallInit.data);
+        console.log("🚀 Data was initialized");
+
+        mongoose.connection.close();
+    } catch (err) {
+        console.log("❌ ERROR:", err);
+    }
 }
 
-const initDB = async () => {
-    await Listing.deleteMany({});
-
-    // initData.data = initData.data.map((obj) => ({...obj,owner: '6831fe2eef63a3852123d1a9'})) 
-    // await Listing.insertMany(initData.data);
-
-     smallInit.data = smallInit.data.map((obj) => ({...obj,owner: '6831fe2eef63a3852123d1a9'})) 
-     await Listing.insertMany(smallInit.data);
-
-    console.log("Data was initialized"); 
-}
-initDB();
+main();
